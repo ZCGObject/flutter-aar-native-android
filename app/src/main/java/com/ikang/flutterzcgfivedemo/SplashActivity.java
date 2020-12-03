3c558e24f7700bd807cd8be2f6896e1985b2d6b4
chenggang.zhang <chenggang.zhang@ikang.com>
2020-12-03 13:52:36 +0800

    fc

---
+++ app/src/main/java/com/ikang/flutterzcgfivedemo/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.ikang.flutterzcgfivedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
